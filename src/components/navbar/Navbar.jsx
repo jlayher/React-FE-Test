09514357a5ce876c6d68f44b52ab3b293250973a
John Layher <johnevanlayher@gmail.com>
2023-01-28 00:18:18 -0500

    Navbar Styled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,80 @@
 //import { logDOM } from '@testing-library/react';
-import React from 'react';
+
+import React, { useState }  from 'react';
 //icons from react icons
 import {RiMenu3Line, RiCloseLine  } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
 
-// BEM -> Block Element Modifier
+// BEM -> Block Element Modifier (what was this comment in reference to?)
+
+
+// syntax?
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>  
+  <p><a href="#wgpt3">What is GPT3?</a></p>  
+  <p><a href="#possibility">Open AI</a></p>  
+  <p><a href="#features">Case Studies</a></p>  
+  <p><a href="#blog">Library</a></p>  
+  </>
+)
 
 export default function Navbar() {
+
+  // toggleMenu is a var holding the false value to begin with.
+  // setToggleMenu is a function that allows us to change the variable's value
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
+    // navbar container
     <div className='gpt3__navbar'>
+      {/* left side. Contains Logo and Menu items */}
       <div className='gpt3__navbar-links'>
+        {/* image container */}
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
+        {/* Container for menu items */}
         <div className='gpt3__navbar-links_container'>
-          <p><a href="#home">Home</a></p>  
-          <p><a href="#wgpt3">What is GPT3?</a></p>  
-          <p><a href="#possibility">Open AI</a></p>  
-          <p><a href="#features">Case Studies</a></p>  
-          <p><a href="#blog">Library</a></p>  
+          <Menu />
         </div>
       </div>
+
+
+      {/* right side. Sign In and Sign Up button*/}
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+
+
+      {/* menu icon */}
+      <div className="gpt3__navbar-menu">
+        {/* toggle whether menu toggle displays as sandwhich or X icon */}
+        {toggleMenu
+          /* if toggleMenu is true, display close line "X".  When Clicked, switch toggleMenu to false, and the menu line executes */
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {/* if toggleMenu = true, render the following dropdown menu*/}
+        {toggleMenu && (
+          /* container that appears like a dropdown from the sandwich */
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+                <div className="gpt3__navbar-menu_container-links-sign">
+                  <p>Sign in</p>
+                  <button type="button">Sign up</button>
+                </div>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* end menu icon */}
+
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+
+// Look into react dom for more information
 import ReactDOM from 'react-dom';
 
+//imports our App.js component, which acts as the parent container for all components
 import App from './App';
+
 //allows us to use basic styling variables in our code
 import './index.css';
+
+//renders our App component and inserts it at the root of the index.html file inside the public folder
 ReactDOM.render(<App />, document.getElementById('root'));
