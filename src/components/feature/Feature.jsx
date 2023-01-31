3042283e87ebae90742594166dd3bcb53fe1d954
John Layher <johnevanlayher@gmail.com>
2023-01-30 22:20:37 -0500

    Features, Possibility, and WhatGPT3 components built and styled

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,6 +3,7 @@ import './feature.css';
 
 export default function Feature({ title, text }) {
   return (
+    // this style appears differently for the topmost feature, and is styled differently for the bottom 3 features.
     <div className='gpt3__features-container__feature'>
       <div className='gpt3__features-container__feature-title'>
         <div />
