4b86ceacb9fb62de93e9ec622812432660f29a24
John Layher <johnevanlayher@gmail.com>
2023-01-31 20:22:17 -0500

    Blog and Articles Finished.  Footer in works

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './article.css';
 
-export default function Article() {
+
+export default function Article({ imgUrl, date, title}) {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container__article '>
+      <div className='gpt3__blog-container__article-image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt3__blog-container__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>  
+        <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
